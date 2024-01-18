1af35ad7b8b83a1d75e16a705d7876a4c2957c3f
justinelut <justinequartz@gmail.com>
2024-01-18 19:37:59 +0300

    added purchase page

---
+++ src/app/buy/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+    <div>Book the web dev package</div>
+    <h3>featureus include</h3>
+    <h3>SEO</h3>
+    <h3>Speed</h3>
+    <h3>Accessibility</h3>
+    </>
+  )
+}
