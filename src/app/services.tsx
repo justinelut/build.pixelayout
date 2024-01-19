8a18154ec32033bfa23b2e39662c75ebcf8b852a
justinelut <justinequartz@gmail.com>
2024-01-19 18:50:43 +0300

    Services component

---
+++ src/app/services.tsx
@@ -0,0 +1,121 @@
+import React from 'react'
+
+export default function Services() {
+  return (
+    <div>
+          <div className="text-emerald-300 text-3xl font-semibold self-center whitespace-nowrap mt-24 max-md:mt-10">
+        Our Services{" "}
+      </div>
+      <div className="text-neutral-950 text-center text-6xl font-semibold max-w-[695px] self-center mt-6 max-md:max-w-full max-md:text-4xl">
+        High-impact services <br />
+        for your business
+      </div>
+      <div className="self-center w-full max-w-[1400px] mt-20 px-5 max-md:max-w-full max-md:mt-10">
+        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+          <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
+            <span className="bg-neutral-950 flex grow flex-col w-full p-11 rounded-3xl items-start max-md:max-w-full max-md:mt-10 max-md:px-5">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/cddc7167cba2e2d63b45513944faeddf2c2662a87ed1fb67bc3939b7d00974ff?apiKey=cc92e77fa59648be96795d005307c4b6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/cddc7167cba2e2d63b45513944faeddf2c2662a87ed1fb67bc3939b7d00974ff?apiKey=cc92e77fa59648be96795d005307c4b6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/cddc7167cba2e2d63b45513944faeddf2c2662a87ed1fb67bc3939b7d00974ff?apiKey=cc92e77fa59648be96795d005307c4b6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/cddc7167cba2e2d63b45513944faeddf2c2662a87ed1fb67bc3939b7d00974ff?apiKey=cc92e77fa59648be96795d005307c4b6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/cddc7167cba2e2d63b45513944faeddf2c2662a87ed1fb67bc3939b7d00974ff?apiKey=cc92e77fa59648be96795d005307c4b6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/cddc7167cba2e2d63b45513944faeddf2c2662a87ed1fb67bc3939b7d00974ff?apiKey=cc92e77fa59648be96795d005307c4b6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/cddc7167cba2e2d63b45513944faeddf2c2662a87ed1fb67bc3939b7d00974ff?apiKey=cc92e77fa59648be96795d005307c4b6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/cddc7167cba2e2d63b45513944faeddf2c2662a87ed1fb67bc3939b7d00974ff?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                className="aspect-square object-contain object-center w-20 fill-zinc-50 overflow-hidden max-w-full"
+              />
+              <div className="text-zinc-100 text-2xl font-medium self-stretch mt-16 max-md:mt-10">
+                Content Marketing{" "}
+              </div>
+              <div className="text-zinc-100 text-lg tracking-wide self-stretch mt-4">
+                Our team creates engaging and shareable content that resonates
+                with your audience, drives organic traffic
+              </div>
+            </span>
+          </div>
+          <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+            <span className="bg-zinc-100 flex grow flex-col w-full px-12 py-11 rounded-3xl items-start max-md:max-w-full max-md:mt-10 max-md:px-5">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/c71ca88204fb8eaf1a86ca613c15c93ca5232f920c4b45ffb1c8d0d6e748e585?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                className="aspect-square object-contain object-center w-20 overflow-hidden max-w-full"
+              />
+              <div className="text-neutral-950 text-2xl font-medium self-stretch mt-16 max-md:mt-10">
+                Graphic Design{" "}
+              </div>
+              <div className="text-neutral-950 text-opacity-90 text-lg tracking-wide self-stretch mt-4">
+                Unlock the power of visual storytelling with our expert graphic
+                design services tailored to elevate your brand and captivate.
+              </div>
+            </span>
+          </div>
+          <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+            <span className="bg-zinc-100 flex grow flex-col w-full p-11 rounded-3xl items-start max-md:max-w-full max-md:mt-10 max-md:px-5">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/e00be509c37e0a99e125a591861fde51195f12493578fcb08eb62baaf5c9e2b8?apiKey=cc92e77fa59648be96795d005307c4b6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/e00be509c37e0a99e125a591861fde51195f12493578fcb08eb62baaf5c9e2b8?apiKey=cc92e77fa59648be96795d005307c4b6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e00be509c37e0a99e125a591861fde51195f12493578fcb08eb62baaf5c9e2b8?apiKey=cc92e77fa59648be96795d005307c4b6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/e00be509c37e0a99e125a591861fde51195f12493578fcb08eb62baaf5c9e2b8?apiKey=cc92e77fa59648be96795d005307c4b6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/e00be509c37e0a99e125a591861fde51195f12493578fcb08eb62baaf5c9e2b8?apiKey=cc92e77fa59648be96795d005307c4b6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/e00be509c37e0a99e125a591861fde51195f12493578fcb08eb62baaf5c9e2b8?apiKey=cc92e77fa59648be96795d005307c4b6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/e00be509c37e0a99e125a591861fde51195f12493578fcb08eb62baaf5c9e2b8?apiKey=cc92e77fa59648be96795d005307c4b6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/e00be509c37e0a99e125a591861fde51195f12493578fcb08eb62baaf5c9e2b8?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                className="aspect-square object-contain object-center w-20 fill-neutral-950 overflow-hidden max-w-full"
+              />
+              <div className="text-neutral-950 text-2xl font-medium self-stretch mt-16 max-md:mt-10">
+                Digital Marketing{" "}
+              </div>
+              <div className="text-neutral-950 text-opacity-90 text-lg tracking-wide self-stretch mt-4">
+                Elevate your brand's online presence with our data-driven
+                digital marketing strategies. From SEO and content marketing
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>{" "}
+      <div className="self-center w-full max-w-[1400px] mt-20 px-5 max-md:max-w-full max-md:mt-10">
+        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+          <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
+            <span className="bg-zinc-100 flex flex-col w-full p-11 rounded-3xl items-start max-md:max-w-full max-md:mt-10 max-md:px-5">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/2ac4fc9635858610f49a6f6f0d9cc3ae19c5f6c52e8001662778ed92bcff9965?apiKey=cc92e77fa59648be96795d005307c4b6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/2ac4fc9635858610f49a6f6f0d9cc3ae19c5f6c52e8001662778ed92bcff9965?apiKey=cc92e77fa59648be96795d005307c4b6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/2ac4fc9635858610f49a6f6f0d9cc3ae19c5f6c52e8001662778ed92bcff9965?apiKey=cc92e77fa59648be96795d005307c4b6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/2ac4fc9635858610f49a6f6f0d9cc3ae19c5f6c52e8001662778ed92bcff9965?apiKey=cc92e77fa59648be96795d005307c4b6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/2ac4fc9635858610f49a6f6f0d9cc3ae19c5f6c52e8001662778ed92bcff9965?apiKey=cc92e77fa59648be96795d005307c4b6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/2ac4fc9635858610f49a6f6f0d9cc3ae19c5f6c52e8001662778ed92bcff9965?apiKey=cc92e77fa59648be96795d005307c4b6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/2ac4fc9635858610f49a6f6f0d9cc3ae19c5f6c52e8001662778ed92bcff9965?apiKey=cc92e77fa59648be96795d005307c4b6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/2ac4fc9635858610f49a6f6f0d9cc3ae19c5f6c52e8001662778ed92bcff9965?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                className="aspect-square object-contain object-center w-20 fill-neutral-950 overflow-hidden max-w-full"
+              />{" "}
+              <div className="text-neutral-950 text-2xl font-medium self-stretch mt-16 max-md:mt-10">
+                Web Design
+              </div>{" "}
+              <div className="text-neutral-950 text-opacity-90 text-lg tracking-wide self-stretch mt-4">
+                We specialize in creating visually stunning, user-friendly
+                websites that align with your brand identity and deliver an
+                exceptional.
+              </div>
+            </span>
+          </div>{" "}
+          <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+            <span className="bg-zinc-100 flex grow flex-col w-full px-12 py-11 rounded-3xl items-start max-md:max-w-full max-md:mt-10 max-md:px-5">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/751e8273d45c1a006bcfbb266b5c84e2a2a9812a9b3e205993774cf8ba6d98ef?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                className="aspect-square object-contain object-center w-20 justify-center items-center overflow-hidden max-w-full"
+              />{" "}
+              <div className="text-neutral-950 text-2xl font-medium self-stretch mt-16 max-md:mt-10">
+                IT Consulting
+              </div>{" "}
+              <div className="text-neutral-950 text-opacity-90 text-lg tracking-wide self-stretch mt-4">
+                IT consulting, or information technology consulting, refers to
+                the practice of providing advisory and implementation services
+              </div>
+            </span>
+          </div>{" "}
+          <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+            <span className="bg-zinc-100 flex flex-col w-full p-11 rounded-3xl items-start max-md:max-w-full max-md:mt-10 max-md:px-5">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/41d01bc43ccc560e5ef54a6e801281141c7b41e0ecfe7e35dab4c0365d28397f?apiKey=cc92e77fa59648be96795d005307c4b6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/41d01bc43ccc560e5ef54a6e801281141c7b41e0ecfe7e35dab4c0365d28397f?apiKey=cc92e77fa59648be96795d005307c4b6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/41d01bc43ccc560e5ef54a6e801281141c7b41e0ecfe7e35dab4c0365d28397f?apiKey=cc92e77fa59648be96795d005307c4b6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/41d01bc43ccc560e5ef54a6e801281141c7b41e0ecfe7e35dab4c0365d28397f?apiKey=cc92e77fa59648be96795d005307c4b6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/41d01bc43ccc560e5ef54a6e801281141c7b41e0ecfe7e35dab4c0365d28397f?apiKey=cc92e77fa59648be96795d005307c4b6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/41d01bc43ccc560e5ef54a6e801281141c7b41e0ecfe7e35dab4c0365d28397f?apiKey=cc92e77fa59648be96795d005307c4b6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/41d01bc43ccc560e5ef54a6e801281141c7b41e0ecfe7e35dab4c0365d28397f?apiKey=cc92e77fa59648be96795d005307c4b6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/41d01bc43ccc560e5ef54a6e801281141c7b41e0ecfe7e35dab4c0365d28397f?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                className="aspect-square object-contain object-center w-20 fill-neutral-950 overflow-hidden max-w-full"
+              />{" "}
+              <div className="text-neutral-950 text-2xl font-medium self-stretch mt-16 max-md:mt-10">
+                Brand Identity{" "}
+              </div>{" "}
+              <div className="text-neutral-950 text-opacity-90 text-lg tracking-wide self-stretch mt-4">
+                It involves creating a unique and recognizable identity that
+                sets the brand apart from competitors and resonates with the
+                target audience.
+              </div>
+            </span>
+          </div>
+        </div>
+      </div>{" "}
+    </div>
+  )
+}
