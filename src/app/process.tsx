074d0543761e2df797f8a9bfa2fbcfc09a2fd9e4
justinelut <justinequartz@gmail.com>
2024-01-20 19:31:25 +0300

    added process section

---
+++ src/app/process.tsx
@@ -0,0 +1,184 @@
+import React from 'react'
+
+export default function Process() {
+  return (
+    <span className="self-center flex w-full max-w-[1660px] flex-col items-center mt-28 px-5 max-md:max-w-full max-md:mt-10">
+    <span className="items-center flex w-[676px] max-w-full flex-col pb-2.5 px-6 max-md:px-5">
+      <div className="text-emerald-300 text-center text-3xl font-semibold whitespace-nowrap">
+        Process
+      </div>{" "}
+      <div className="text-neutral-950 text-center text-6xl font-semibold self-stretch mt-6 max-md:max-w-full max-md:text-4xl">
+        Process that moves <br />
+        things forward
+      </div>
+    </span>{" "}
+    <div className="self-stretch mt-20 max-md:max-w-full max-md:mt-10">
+      <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+        <div className="flex flex-col items-stretch w-[68%] max-md:w-full max-md:ml-0">
+          <div className="grow px-px max-md:max-w-full max-md:mt-10">
+            <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+              <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/421c46a3429fb34db3094a1e827e882dbc7e185415a1256ae7e079d7d617429c?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                  className="aspect-[1.33] object-contain object-center w-full overflow-hidden max-md:mt-6"
+                />
+              </div>{" "}
+              <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+                <div className="flex-col text-neutral-950 text-opacity-90 text-base tracking-wide relative overflow-hidden aspect-[1.3650793650793651] mt-1 pl-10 pr-16 pt-28 pb-8 items-start max-md:mt-7 max-md:pt-10 max-md:px-5">
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/6e94fcc9a4e4829907b1a7db36488d6bf147934602ff3e12ab466623f832289f?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                    className="absolute h-full w-full object-cover object-center inset-0"
+                  />{" "}
+                  Research is a critical component of the design process,
+                  helping designers understand the problem
+                </div>
+              </div>{" "}
+              <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/bc1f663c8dd94d452bab847d1fcd1ec1dcc4f7024c5baacb9723f53e4b5c718e?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                  className="aspect-[1.3] object-contain object-center w-full overflow-hidden grow mt-1 max-md:mt-7"
+                />
+              </div>
+            </div>
+          </div>
+        </div>{" "}
+        <div className="flex flex-col items-stretch w-[32%] ml-5 max-md:w-full max-md:ml-0">
+          <span className="flex flex-col mt-1 items-end max-md:max-w-full max-md:mt-10">
+            <div className="flex-col text-neutral-950 text-opacity-90 text-center text-3xl font-semibold relative whitespace-nowrap overflow-hidden min-h-[83px] w-[484px] max-w-full justify-center pl-28 pr-16 py-7 items-start max-md:pl-8 max-md:pr-5">
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/5372339dd932de6cc1de11243e8986e8e95097c698e0117bc6cd5cda7b2971e9?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                className="absolute h-full w-full object-cover object-center inset-0"
+              />{" "}
+              Testing
+            </div>{" "}
+            <div className="text-neutral-950 text-opacity-90 text-base tracking-wide self-stretch mt-8 max-md:max-w-full">
+              Testing is a crucial phase in the design process to ensure
+              that the product or system meets the specified requirements
+            </div>
+          </span>
+        </div>
+      </div>
+    </div>{" "}
+    <div className="text-neutral-950 text-6xl font-semibold mt-28 max-md:max-w-full max-md:text-4xl max-md:mt-10">
+      Recent Showcase
+    </div>{" "}
+    <div className="self-stretch mt-24 max-md:max-w-full max-md:mt-10">
+      <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+        <div className="flex flex-col items-stretch w-6/12 max-md:w-full max-md:ml-0">
+          <span className="flex flex-col max-md:max-w-full max-md:mt-10">
+            <img
+              loading="lazy"
+              srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/9d2912068f57d219acd6a611efe78a29d3d23f3c3bba024a9e4f33489577ee4e?apiKey=cc92e77fa59648be96795d005307c4b6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/9d2912068f57d219acd6a611efe78a29d3d23f3c3bba024a9e4f33489577ee4e?apiKey=cc92e77fa59648be96795d005307c4b6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9d2912068f57d219acd6a611efe78a29d3d23f3c3bba024a9e4f33489577ee4e?apiKey=cc92e77fa59648be96795d005307c4b6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/9d2912068f57d219acd6a611efe78a29d3d23f3c3bba024a9e4f33489577ee4e?apiKey=cc92e77fa59648be96795d005307c4b6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/9d2912068f57d219acd6a611efe78a29d3d23f3c3bba024a9e4f33489577ee4e?apiKey=cc92e77fa59648be96795d005307c4b6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9d2912068f57d219acd6a611efe78a29d3d23f3c3bba024a9e4f33489577ee4e?apiKey=cc92e77fa59648be96795d005307c4b6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/9d2912068f57d219acd6a611efe78a29d3d23f3c3bba024a9e4f33489577ee4e?apiKey=cc92e77fa59648be96795d005307c4b6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/9d2912068f57d219acd6a611efe78a29d3d23f3c3bba024a9e4f33489577ee4e?apiKey=cc92e77fa59648be96795d005307c4b6&"
+              className="aspect-[1.76] object-contain object-center w-full overflow-hidden self-stretch max-md:max-w-full"
+            />{" "}
+            <div className="text-neutral-950 text-center text-3xl font-semibold mt-4 self-start">
+              Web UI design
+            </div>{" "}
+            <div className="text-neutral-950 text-opacity-90 text-center text-lg tracking-wide mt-2.5 self-start">
+              Creative UI design
+            </div>{" "}
+            <div className="flex-col overflow-hidden self-stretch relative flex min-h-[450px] w-full mt-24 pr-20 pb-12 items-start max-md:max-w-full max-md:mt-10 max-md:pr-5">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/ff5b1a9c09ce51b38d7da39ebca9ef1ac7f6298081fccabe5246c7af79fed8e0?apiKey=cc92e77fa59648be96795d005307c4b6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/ff5b1a9c09ce51b38d7da39ebca9ef1ac7f6298081fccabe5246c7af79fed8e0?apiKey=cc92e77fa59648be96795d005307c4b6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ff5b1a9c09ce51b38d7da39ebca9ef1ac7f6298081fccabe5246c7af79fed8e0?apiKey=cc92e77fa59648be96795d005307c4b6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/ff5b1a9c09ce51b38d7da39ebca9ef1ac7f6298081fccabe5246c7af79fed8e0?apiKey=cc92e77fa59648be96795d005307c4b6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/ff5b1a9c09ce51b38d7da39ebca9ef1ac7f6298081fccabe5246c7af79fed8e0?apiKey=cc92e77fa59648be96795d005307c4b6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/ff5b1a9c09ce51b38d7da39ebca9ef1ac7f6298081fccabe5246c7af79fed8e0?apiKey=cc92e77fa59648be96795d005307c4b6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/ff5b1a9c09ce51b38d7da39ebca9ef1ac7f6298081fccabe5246c7af79fed8e0?apiKey=cc92e77fa59648be96795d005307c4b6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/ff5b1a9c09ce51b38d7da39ebca9ef1ac7f6298081fccabe5246c7af79fed8e0?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                className="absolute h-full w-full object-cover object-center inset-0"
+              />{" "}
+              <div className="relative text-neutral-950 text-center text-3xl font-semibold">
+                Graphic design{" "}
+              </div>{" "}
+              <div className="relative text-neutral-950 text-opacity-90 text-center text-xl mt-4 mb-64 max-md:mb-10">
+                Graphic design{" "}
+              </div>
+            </div>{" "}
+            <div className="text-neutral-950 text-center text-3xl font-semibold mt-6 self-start">
+              UI Design
+            </div>{" "}
+            <div className="text-neutral-950 text-opacity-90 text-center text-lg tracking-wide mt-2 self-start">
+              Creative Rebranding for logo
+            </div>
+          </span>
+        </div>{" "}
+        <div className="flex flex-col items-stretch w-6/12 ml-5 max-md:w-full max-md:ml-0">
+          <span className="flex grow flex-col mt-20 max-md:max-w-full max-md:mt-10">
+            <span className="justify-center items-stretch bg-neutral-950 flex gap-2.5 p-2.5 rounded-3xl self-end">
+              <div className="items-center flex aspect-[1.6428571428571428] flex-col justify-center pr-5">
+                <img
+                  loading="lazy"
+                  srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/fbbb23805a4b5f198473c91b9b2d7beb6c7f751ed786b52a2e38e0145e260e69?apiKey=cc92e77fa59648be96795d005307c4b6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/fbbb23805a4b5f198473c91b9b2d7beb6c7f751ed786b52a2e38e0145e260e69?apiKey=cc92e77fa59648be96795d005307c4b6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/fbbb23805a4b5f198473c91b9b2d7beb6c7f751ed786b52a2e38e0145e260e69?apiKey=cc92e77fa59648be96795d005307c4b6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/fbbb23805a4b5f198473c91b9b2d7beb6c7f751ed786b52a2e38e0145e260e69?apiKey=cc92e77fa59648be96795d005307c4b6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/fbbb23805a4b5f198473c91b9b2d7beb6c7f751ed786b52a2e38e0145e260e69?apiKey=cc92e77fa59648be96795d005307c4b6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/fbbb23805a4b5f198473c91b9b2d7beb6c7f751ed786b52a2e38e0145e260e69?apiKey=cc92e77fa59648be96795d005307c4b6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/fbbb23805a4b5f198473c91b9b2d7beb6c7f751ed786b52a2e38e0145e260e69?apiKey=cc92e77fa59648be96795d005307c4b6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/fbbb23805a4b5f198473c91b9b2d7beb6c7f751ed786b52a2e38e0145e260e69?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                  className="aspect-square object-contain object-center w-7 overflow-hidden rounded-[50%]"
+                />
+              </div>{" "}
+              <div className="text-zinc-50 text-xl grow shrink basis-auto self-start">
+                Start your Free Trial
+              </div>
+            </span>{" "}
+            <div className="flex-col overflow-hidden self-stretch relative flex min-h-[450px] w-full justify-center items-center mt-12 px-16 py-12 max-md:mt-10 max-md:px-5">
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/428776167eb681ef3d546071d8f3d0c2760b54417d5dba29647f051e780c2288?apiKey=cc92e77fa59648be96795d005307c4b6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/428776167eb681ef3d546071d8f3d0c2760b54417d5dba29647f051e780c2288?apiKey=cc92e77fa59648be96795d005307c4b6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/428776167eb681ef3d546071d8f3d0c2760b54417d5dba29647f051e780c2288?apiKey=cc92e77fa59648be96795d005307c4b6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/428776167eb681ef3d546071d8f3d0c2760b54417d5dba29647f051e780c2288?apiKey=cc92e77fa59648be96795d005307c4b6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/428776167eb681ef3d546071d8f3d0c2760b54417d5dba29647f051e780c2288?apiKey=cc92e77fa59648be96795d005307c4b6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/428776167eb681ef3d546071d8f3d0c2760b54417d5dba29647f051e780c2288?apiKey=cc92e77fa59648be96795d005307c4b6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/428776167eb681ef3d546071d8f3d0c2760b54417d5dba29647f051e780c2288?apiKey=cc92e77fa59648be96795d005307c4b6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/428776167eb681ef3d546071d8f3d0c2760b54417d5dba29647f051e780c2288?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                className="absolute h-full w-full object-cover object-center inset-0"
+              />{" "}
+              <img
+                loading="lazy"
+                srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/cdf31d139053026d9e38dbd1acf4d1dffea655eedd64050bbacca476f69ebcfc?apiKey=cc92e77fa59648be96795d005307c4b6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/cdf31d139053026d9e38dbd1acf4d1dffea655eedd64050bbacca476f69ebcfc?apiKey=cc92e77fa59648be96795d005307c4b6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/cdf31d139053026d9e38dbd1acf4d1dffea655eedd64050bbacca476f69ebcfc?apiKey=cc92e77fa59648be96795d005307c4b6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/cdf31d139053026d9e38dbd1acf4d1dffea655eedd64050bbacca476f69ebcfc?apiKey=cc92e77fa59648be96795d005307c4b6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/cdf31d139053026d9e38dbd1acf4d1dffea655eedd64050bbacca476f69ebcfc?apiKey=cc92e77fa59648be96795d005307c4b6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/cdf31d139053026d9e38dbd1acf4d1dffea655eedd64050bbacca476f69ebcfc?apiKey=cc92e77fa59648be96795d005307c4b6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/cdf31d139053026d9e38dbd1acf4d1dffea655eedd64050bbacca476f69ebcfc?apiKey=cc92e77fa59648be96795d005307c4b6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/cdf31d139053026d9e38dbd1acf4d1dffea655eedd64050bbacca476f69ebcfc?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                className="aspect-square object-contain object-center w-[50px] overflow-hidden max-w-full mt-36 mb-28 rounded-[50%] max-md:my-10"
+              />
+            </div>{" "}
+            <div className="text-neutral-950 text-center text-3xl font-semibold mt-4 self-start max-md:max-w-full">
+              To design Digital Strategy
+            </div>{" "}
+            <div className="text-neutral-950 text-opacity-90 text-center text-lg tracking-wide mt-2 self-start max-md:max-w-full">
+              Social Media Marketing
+            </div>{" "}
+            <img
+              loading="lazy"
+              srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/1fdb5fbd13c247174ed1fcb0f52c06d6317501249e3354e55e1146f4c9e1aaac?apiKey=cc92e77fa59648be96795d005307c4b6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fdb5fbd13c247174ed1fcb0f52c06d6317501249e3354e55e1146f4c9e1aaac?apiKey=cc92e77fa59648be96795d005307c4b6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fdb5fbd13c247174ed1fcb0f52c06d6317501249e3354e55e1146f4c9e1aaac?apiKey=cc92e77fa59648be96795d005307c4b6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fdb5fbd13c247174ed1fcb0f52c06d6317501249e3354e55e1146f4c9e1aaac?apiKey=cc92e77fa59648be96795d005307c4b6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fdb5fbd13c247174ed1fcb0f52c06d6317501249e3354e55e1146f4c9e1aaac?apiKey=cc92e77fa59648be96795d005307c4b6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fdb5fbd13c247174ed1fcb0f52c06d6317501249e3354e55e1146f4c9e1aaac?apiKey=cc92e77fa59648be96795d005307c4b6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fdb5fbd13c247174ed1fcb0f52c06d6317501249e3354e55e1146f4c9e1aaac?apiKey=cc92e77fa59648be96795d005307c4b6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/1fdb5fbd13c247174ed1fcb0f52c06d6317501249e3354e55e1146f4c9e1aaac?apiKey=cc92e77fa59648be96795d005307c4b6&"
+              className="aspect-[1.76] object-contain object-center w-full overflow-hidden self-stretch mt-24 max-md:max-w-full max-md:mt-10"
+            />{" "}
+            <div className="text-neutral-950 text-center text-3xl font-semibold mt-4 self-start">
+              UI Design
+            </div>{" "}
+            <div className="text-neutral-950 text-opacity-90 text-center text-lg tracking-wide mt-2 self-start">
+              Creative Rebranding for logo
+            </div>
+          </span>
+        </div>
+      </div>
+    </div>{" "}
+    <span className="items-center bg-zinc-100 self-stretch flex flex-col mt-24 p-11 rounded-3xl max-md:max-w-full max-md:mt-10 max-md:px-5">
+      <img
+        loading="lazy"
+        srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/a1f8bb5f0d977f9a5166ce7f0dd64465d842cf550283166f68fe961df8419a88?apiKey=cc92e77fa59648be96795d005307c4b6&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/a1f8bb5f0d977f9a5166ce7f0dd64465d842cf550283166f68fe961df8419a88?apiKey=cc92e77fa59648be96795d005307c4b6&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/a1f8bb5f0d977f9a5166ce7f0dd64465d842cf550283166f68fe961df8419a88?apiKey=cc92e77fa59648be96795d005307c4b6&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/a1f8bb5f0d977f9a5166ce7f0dd64465d842cf550283166f68fe961df8419a88?apiKey=cc92e77fa59648be96795d005307c4b6&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/a1f8bb5f0d977f9a5166ce7f0dd64465d842cf550283166f68fe961df8419a88?apiKey=cc92e77fa59648be96795d005307c4b6&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/a1f8bb5f0d977f9a5166ce7f0dd64465d842cf550283166f68fe961df8419a88?apiKey=cc92e77fa59648be96795d005307c4b6&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/a1f8bb5f0d977f9a5166ce7f0dd64465d842cf550283166f68fe961df8419a88?apiKey=cc92e77fa59648be96795d005307c4b6&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/a1f8bb5f0d977f9a5166ce7f0dd64465d842cf550283166f68fe961df8419a88?apiKey=cc92e77fa59648be96795d005307c4b6&"
+        className="aspect-square object-contain object-center w-[150px] overflow-hidden self-center max-w-full rounded-[50%]"
+      />{" "}
+      <div className="text-neutral-950 text-center text-2xl font-bold self-stretch mt-9 max-md:max-w-full">
+        “Be genuine in your assessment, and provide constructive feedback to
+        benefit both potential customers and the company providing the
+        product or service.”
+      </div>{" "}
+      <div className="text-neutral-950 text-center text-xl font-semibold self-center mt-9 max-md:max-w-full">
+        Jacqueline Miller
+      </div>{" "}
+      <div className="text-neutral-950 text-opacity-90 text-center text-base tracking-wide self-center mt-1.5 max-md:max-w-full">
+        CEO of an eduport
+      </div>{" "}
+      <div className="justify-center items-stretch self-center flex w-[242px] max-w-full gap-3 mt-5 pr-20 max-md:pr-5">
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/67af8adb7066040454d54d189f5428da6a7afa2e9a63b39b3d48f33564370f2b?apiKey=cc92e77fa59648be96795d005307c4b6&"
+          className="aspect-square object-contain object-center w-full overflow-hidden shrink-0 flex-1"
+        />{" "}
+        <img
+          loading="lazy"
+          src="https://cdn.builder.io/api/v1/image/assets/TEMP/ce9bfce06504329c386ec74f9092c783bdbf7617df37f864a7a09a6a517277a8?apiKey=cc92e77fa59648be96795d005307c4b6&"
+          className="aspect-square object-contain object-center w-full overflow-hidden shrink-0 flex-1"
+        />
+      </div>
+    </span>
+  </span>
+  )
+}
