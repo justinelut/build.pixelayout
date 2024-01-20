adb4c9555219e30da2d0f449d1662cb8fe664e34
justinelut <justinequartz@gmail.com>
2024-01-20 11:24:06 +0300

    added pricing page

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,160 @@
+
+
+export default function page() {
+  return (
+    <div className="bg-violet-900 flex flex-col justify-center items-center px-16 py-12 max-md:px-5">
+      <span className="flex w-full max-w-[1069px] flex-col mt-20 mb-14 max-md:max-w-full max-md:my-10">
+        <div className="text-white text-6xl font-bold tracking-tighter self-stretch max-md:max-w-full max-md:text-4xl">
+          Transparent Pricing
+        </div>
+        <div className="text-white text-xl tracking-tight self-stretch mt-6 max-md:max-w-full">
+          Pricing built for people just like you.
+        </div>
+        <div className="flex w-[616px] max-w-full justify-between gap-5 mt-32 self-start items-start max-md:flex-wrap max-md:mt-10">
+          <span className="flex flex-col items-stretch">
+            <div className="text-white text-4xl font-bold">Free</div>
+            <div className="text-white text-base whitespace-nowrap mt-6">
+              Limtless possibilites
+            </div>
+          </span>
+          <span className="flex flex-col items-stretch mt-1">
+            <div className="text-white text-4xl font-bold whitespace-nowrap">
+              Unlimited
+            </div>
+            <div className="text-white text-base whitespace-nowrap mt-6">
+              Limtless possibilites
+            </div>
+          </span>
+        </div>
+        <div className="self-stretch mt-7 max-md:max-w-full">
+          <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+            <div className="flex flex-col items-stretch w-[41%] max-md:w-full max-md:ml-0">
+              <span className="shadow-xl bg-white flex flex-col w-full pl-10 pr-20 py-11 rounded-3xl items-start max-md:max-w-full max-md:mt-8 max-md:px-5">
+                <div className="text-black text-4xl font-bold">
+                  $0<span className="text-xl">/mo</span>
+                </div>
+                <div className="text-black text-base mt-6">
+                  Try it as long as you like
+                </div>
+                <span className="flex gap-3 mt-11 items-start max-md:mt-10">
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/872d8188f5685ba96e080be6960cae07a634e8025d7cf470b9817a41e1682ee8?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                    className="aspect-[1.15] object-contain object-center w-[15px] overflow-hidden shrink-0 max-w-full"
+                  />
+                  <div className="text-black text-base">
+                    Feature label goes here
+                  </div>
+                </span>
+                <span className="flex gap-3 mt-6 items-start">
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/872d8188f5685ba96e080be6960cae07a634e8025d7cf470b9817a41e1682ee8?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                    className="aspect-[1.15] object-contain object-center w-[15px] overflow-hidden shrink-0 max-w-full"
+                  />
+                  <div className="text-black text-base">
+                    Feature label goes here
+                  </div>
+                </span>
+                <span className="flex gap-3 mt-6 items-start">
+                  <img
+                    loading="lazy"
+                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/872d8188f5685ba96e080be6960cae07a634e8025d7cf470b9817a41e1682ee8?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                    className="aspect-[1.15] object-contain object-center w-[15px] overflow-hidden shrink-0 max-w-full"
+                  />
+                  <div className="text-black text-base">
+                    Feature label goes here
+                  </div>
+                </span>
+                <span className="text-violet-900 text-center text-sm font-bold tracking-tight whitespace-nowrap bg-purple-50 w-[257px] max-w-full justify-center items-center mt-20 px-16 py-5 rounded-3xl max-md:mt-10 max-md:px-5">
+                  Sign up for free
+                </span>
+              </span>
+            </div>
+            <div className="flex flex-col items-stretch w-[59%] ml-5 max-md:w-full max-md:ml-0">
+              <span className="shadow-xl bg-violet-950 flex grow flex-col w-full mt-1 pl-10 pr-20 py-11 rounded-3xl items-start max-md:max-w-full max-md:mt-9 max-md:px-5">
+                <div className="text-white text-4xl font-bold max-md:max-w-full">
+                  $49<span className="text-xl">/mo</span>
+                </div>
+                <div className="text-white text-base mt-6 max-md:max-w-full">
+                  Try it as long as you like
+                </div>
+                <div className="flex w-[435px] max-w-full items-stretch justify-between gap-5 mt-11 max-md:flex-wrap max-md:mt-10">
+                  <span className="flex justify-between gap-3 items-start">
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/8a1bb9287fbcee1a61df52a6293685d2eae12388f19890344b6d715fd3341600?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                      className="aspect-[1.15] object-contain object-center w-[15px] overflow-hidden shrink-0 max-w-full"
+                    />
+                    <div className="text-white text-base grow whitespace-nowrap">
+                      Feature label goes here
+                    </div>
+                  </span>
+                  <span className="flex justify-between gap-3 items-start">
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/8a1bb9287fbcee1a61df52a6293685d2eae12388f19890344b6d715fd3341600?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                      className="aspect-[1.15] object-contain object-center w-[15px] overflow-hidden shrink-0 max-w-full"
+                    />
+                    <div className="text-white text-base grow whitespace-nowrap">
+                      Feature label goes here
+                    </div>
+                  </span>
+                </div>
+                <div className="flex w-[435px] max-w-full items-stretch justify-between gap-5 mt-6 max-md:flex-wrap">
+                  <span className="flex justify-between gap-3 items-start">
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/8a1bb9287fbcee1a61df52a6293685d2eae12388f19890344b6d715fd3341600?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                      className="aspect-[1.15] object-contain object-center w-[15px] overflow-hidden shrink-0 max-w-full"
+                    />
+                    <div className="text-white text-base grow whitespace-nowrap">
+                      Feature label goes here
+                    </div>
+                  </span>
+                  <span className="flex justify-between gap-3 items-start">
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/8a1bb9287fbcee1a61df52a6293685d2eae12388f19890344b6d715fd3341600?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                      className="aspect-[1.15] object-contain object-center w-[15px] overflow-hidden shrink-0 max-w-full"
+                    />
+                    <div className="text-white text-base grow whitespace-nowrap">
+                      Feature label goes here
+                    </div>
+                  </span>
+                </div>
+                <div className="flex w-[435px] max-w-full items-stretch justify-between gap-5 mt-6 max-md:flex-wrap">
+                  <span className="flex justify-between gap-3 items-start">
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/8a1bb9287fbcee1a61df52a6293685d2eae12388f19890344b6d715fd3341600?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                      className="aspect-[1.15] object-contain object-center w-[15px] overflow-hidden shrink-0 max-w-full"
+                    />
+                    <div className="text-white text-base grow whitespace-nowrap">
+                      Feature label goes here
+                    </div>
+                  </span>
+                  <span className="flex justify-between gap-3 items-start">
+                    <img
+                      loading="lazy"
+                      src="https://cdn.builder.io/api/v1/image/assets/TEMP/8a1bb9287fbcee1a61df52a6293685d2eae12388f19890344b6d715fd3341600?apiKey=cc92e77fa59648be96795d005307c4b6&"
+                      className="aspect-[1.15] object-contain object-center w-[15px] overflow-hidden shrink-0 max-w-full"
+                    />
+                    <div className="text-white text-base grow whitespace-nowrap">
+                      Feature label goes here
+                    </div>
+                  </span>
+                </div>
+                <span className="text-violet-900 text-center text-sm font-bold tracking-tight bg-purple-50 w-[435px] max-w-full justify-center items-center mt-20 px-16 py-5 rounded-3xl max-md:mt-10 max-md:px-5">
+                  Sign up for free
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </span>
+    </div>
+  );
+}
+
+
